0b8c62d1ba34ce178e79ce158fae692fa0419d7e
Fatih <fatihzkn@web.de>
2023-10-30 15:55:52 +0100

    Commit- 8

---
+++ src/Part10/AverageOfNumbers.java
@@ -0,0 +1,31 @@
+package Part10;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class AverageOfNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> inputs = new ArrayList<>();
+
+        System.out.println("Input numbers, type 'end' to stop.");
+        while(true) {
+            String input = scanner.nextLine();
+
+            if(input.equals("end")) {
+                break;
+            }
+
+            inputs.add(input);
+        }
+
+        double average = inputs.stream()
+                .mapToDouble(s -> Integer.valueOf(s))
+                .average()
+                .getAsDouble();
+
+        System.out.println("average of the numbers. " + average);
+
+    }
+}
