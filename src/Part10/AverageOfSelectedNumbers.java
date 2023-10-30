51f7c85753204f0c105b3dd808cf177bd639fc9d
Fatih <fatihzkn@web.de>
2023-10-30 15:56:08 +0100

    Commit - 8

---
+++ src/Part10/AverageOfSelectedNumbers.java
@@ -0,0 +1,45 @@
+package Part10;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class AverageOfSelectedNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> inputs = new ArrayList<>();
+
+        System.out.println("Input numbers, type 'end' to stop.");
+        while(true) {
+            String input = scanner.nextLine();
+
+            if(input.equals("end")) {
+                break;
+            }
+
+            inputs.add(input);
+        }
+
+        System.out.println("Print the average of the negative numbers or the positive numbers? (n/p)");
+        String input = scanner.nextLine();
+
+        if(input.equals("n")) {
+            double average = inputs.stream()
+                    .mapToInt(s -> Integer.valueOf(s))
+                    .filter(number -> number < 0)
+                    .average()
+                    .getAsDouble();
+
+            System.out.println("average of the negative numbers. " + average);
+
+        } else {
+            double average = inputs.stream()
+                    .mapToInt(s -> Integer.valueOf(s))
+                    .filter(number -> number >= 0)
+                    .average()
+                    .getAsDouble();
+
+            System.out.println("average of the positive numbers. " + average);
+        }
+    }
+}
